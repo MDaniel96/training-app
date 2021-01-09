920d4a81f27709826ae880bbf3ed51ca22b71780
akos989 <morvaiakos1998@gmail.com>
2021-01-09 15:21:54 +0100

    refactor(one-module): Pages in one module #3

--- training-app/src/app/app.module.ts
+++ training-app/src/app/app.module.ts
@@ -1,24 +1,48 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
-import {RouteReuseStrategy} from '@angular/router';
+import {RouteReuseStrategy, RouterModule} from '@angular/router';
 
 import {IonicModule, IonicRouteStrategy} from '@ionic/angular';
 import {SplashScreen} from '@ionic-native/splash-screen/ngx';
 import {StatusBar} from '@ionic-native/status-bar/ngx';
 
-import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
+import {CommonModule} from '@angular/common';
+import {FormsModule} from '@angular/forms';
+import {Tab1Page} from './pages/tab1/tab1.page';
+import {Tab2Page} from './pages/tab2/tab2.page';
+import {Tab3Page} from './pages/tab3/tab3.page';
+import {TabsComponent} from './components/tabs/tabs.component';
+import {AppPageRoutingModule} from './app-routing.module';
+import {Tab4Page} from './pages/tab4/tab4.page';
+import {Tab5Page} from './pages/tab5/tab5.page';
 
 @NgModule({
-  declarations: [AppComponent],
-  entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [
-    StatusBar,
-    SplashScreen,
-    {provide: RouteReuseStrategy, useClass: IonicRouteStrategy}
-  ],
-  bootstrap: [AppComponent]
+    imports: [
+        BrowserModule,
+        IonicModule.forRoot(),
+        IonicModule,
+        CommonModule,
+        FormsModule,
+        AppPageRoutingModule
+    ],
+    declarations: [
+        AppComponent,
+        TabsComponent,
+        Tab1Page,
+        Tab2Page,
+        Tab3Page,
+        Tab4Page,
+        Tab5Page
+    ],
+    providers: [
+        StatusBar,
+        SplashScreen,
+        {provide: RouteReuseStrategy, useClass: IonicRouteStrategy}
+    ],
+    entryComponents: [],
+    bootstrap: [AppComponent],
+    exports: [RouterModule]
 })
 export class AppModule {
 }
